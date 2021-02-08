7e7445162f7d2535eb87b52ca5dea0276eabe8be
Ying Chen <yingcheny2017@gmail.com>
2021-02-07 18:12:20 -0800

    Gulp - clean up

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ https://gulpjs.com/docs/en/getting-started/quick-start
 ## Notes
 - Each gulp task is an asynchronous JS fn
 - Tasks - public vs. private
--- Public: exported, can be ran by Gulp CLI
--- Private: used internally, used as part of series() or parallel()
-- series() and parallel() can be nested to any arbitrary depth.
 
-### Functions/APIs provided by Gulp
-#### Series
-- series()
+    - Public: exported, can be ran by Gulp CLI
+    - Private: used internally, used as part of series() or parallel()
+    - series() and parallel() can be nested to any arbitrary depth.
+
+### Functions / APIs provided by Gulp
+#### series()
 - Execute tasks in order
 
 ### parallel()
@@ -19,7 +19,7 @@ https://gulpjs.com/docs/en/getting-started/quick-start
 ### src(<glob>) / dest(<glob>) - used to interact with system files
 - glob: a string of literal and/or wildcard characters used to match filepaths.
 
-### pipe(<plugins>())
+### pipe(plugin())
 
 ### watch()
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 const { 
 	series, 
 	parallel, 
@@ -13,70 +12,65 @@ const babel = require('gulp-babel');
 // The `clean` function is not exported so it can be considered a private task.
 // It can still be used within the `series()` composition.
 function clean(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 // The `build` function is exported so it is public and can be run with the `gulp` command.
 // It can also be used within the `series()` composition.
 function build(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function transpile(cb) {
 	// body omitted
 	cb();
 }
-  
+	
 function bundle(cb) {
 	// body omitted
 	cb();
 }  
 
 function javascript(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function css(cb) {
-  // body omitted
-  cb();
-}
-
-function clean(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function cssTranspile(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function cssMinify(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function jsTranspile(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function jsBundle(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function jsMinify(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function publish(cb) {
-  // body omitted
-  cb();
+	// body omitted
+	cb();
 }
 
 function srcDestTask () {
@@ -86,13 +80,13 @@ function srcDestTask () {
 }
 
 exports.build = series(
-  clean,
-  parallel(
-    cssTranspile,
-    series(jsTranspile, jsBundle)
-  ),
-  parallel(cssMinify, jsMinify),
-  publish
+	clean,
+	parallel(
+		cssTranspile,
+		series(jsTranspile, jsBundle)
+	),
+	parallel(cssMinify, jsMinify),
+	publish
 );
 
 // exports.build = series(transpile, bundle);
